f3975214cc8c21789a3dc0ab805a224b566be4a7
Bright Gong <bright.gong01@sap.com>
2022-11-10 23:15:55 +0800

    make the build fail

--- src/main/java/com/example/demo/controller/HomePage.java
+++ src/main/java/com/example/demo/controller/HomePage.java
@@ -4,7 +4,6 @@ import org.springframework.lang.Nullable;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,7 +17,7 @@ public class HomePage {
     if (ObjectUtils.isEmpty(username)) {
       return "Hello, world!";
     } else {
-      return "Hello, " + username;
+      return "Hello, " + username + "!";
     }
   }
 }
