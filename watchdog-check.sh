3646cd7c258b05b5b10813eb99e308407b840e80
Bright Gong <3610576+brightgong@users.noreply.github.com>
2022-12-15 15:35:59 +0800

    Add files via upload

---
+++ watchdog-check.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+code=$(curl -k -o /dev/null -s -w "%{http_code}\n" https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/health)
+echo "response code is ${code}"
+if [ $code -eq 503 ]; then
+  echo "start unseal ..."
+  curl -k --request POST https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/unseal --data "{\"key\":\"${UNSEALKEY_1}\"}"
+  curl -k --request POST https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/unseal --data "{\"key\":\"${UNSEALKEY_2}\"}"
+  curl -k --request POST https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/unseal --data "{\"key\":\"${UNSEALKEY_3}\"}"
+  curl -k --request POST https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/unseal --data "{\"key\":\"${UNSEALKEY_4}\"}"
+  curl -k --request POST https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/unseal --data "{\"key\":\"${UNSEALKEY_5}\"}"
+fi
+code=$(curl -k -o /dev/null -s -w "%{http_code}\n" https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/health)
+echo "response code is ${code}"
+if [ $code -eq 200 ]; then
+  echo "auto unseal success"
+  sleep 3600
+else
+  curl -k https://${VAULT_AUTOUNSEAL_SERVICE_HOST}:${VAULT_AUTOUNSEAL_SERVICE_PORT}/v1/sys/health
+  exit -1
+fi
